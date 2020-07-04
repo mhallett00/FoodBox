638f12c3a8ac53f71b2ec9261b2e09b221207c25
Michael Hallett <m.hallett@gmail.com>
2020-07-04 14:20:48 -0400

    initial set up of react and express with pg and knex

---
+++ api/server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+const knex = require('./knex/knex.js');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.get('/tasks', (req, res) => {
+  // use the knex variable above to create dynamic queries
+});
+
+app.get('/api/greeting', (req, res) => {
+  const name = req.query.name || 'World';
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+});
+
+module.exports = app;
