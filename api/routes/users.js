0a4f1f410a50049449d7f541ffb8be119b2ba206
Michael Hallett <m.hallett@gmail.com>
2020-07-06 16:11:12 -0400

    Added basic users back-end route with static seed

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
 
+// static data
+const users = [
+  {
+    first_name: "Francis",
+    last_name: "Bourgouin",
+    email: "fbourgouin@fakemail.com",
+    password_digest: "example",
+    is_seller: false,
+    seller_postcode: ""
+  },
+  {
+    first_name: "Dana",
+    last_name: "Lafleur",
+    email: "dlafleur@fakemail.com",
+    password_digest: "example",
+    is_seller: true,
+    seller_postcode: "H2T 2L1"
+  },
+  {
+    first_name: "Albert",
+    last_name: "Jacques",
+    email: "ajacques@fakemail.com",
+    password_digest: "example",
+    is_seller: true,
+    seller_postcode: "H2J 1E5"
+  },
+  {
+    first_name: "Avril",
+    last_name: "Devereux",
+    email: "adevereux@fakemail.com",
+    password_digest: "example",
+    is_seller: true,
+    seller_postcode: "H2A 3M4"
+  }
+]
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  
+  res.json(users);
+  console.log("User data sent");
 });
 
 module.exports = router;
