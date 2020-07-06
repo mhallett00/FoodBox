56b41d4692c9c003a05d1b9cbc3be73a0b0fd80e
Michael Hallett <m.hallett@gmail.com>
2020-07-06 16:10:06 -0400

    Added initial project relevant seed data

--- api/knex/seeds/01_users.js
+++ api/knex/seeds/01_users.js
@@ -1,24 +1,42 @@
-exports.seed = function(knex, Promise) {
+
+exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('users').del()
-  .then(function () {
-    // Inserts seed entries
-    return knex('users').insert([
-      {
-        id: 1,
-        email: 'nigel@email.com',
-        password: 'dorwssap'
-      },
-      {
-        id: 2,
-        email: 'nakaz@email.com',
-        password: 'password1'
-      },
-      {
-        id: 3,
-        email: 'jaywon@email.com',
-        password: 'password123'
-      }
-    ]);
-  });
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {
+          first_name: "Francis",
+          last_name: "Bourgouin",
+          email: "fbourgouin@fakemail.com",
+          password_digest: "example",
+          is_seller: false,
+          seller_postcode: ""
+        },
+        {
+          first_name: "Dana",
+          last_name: "Lafleur",
+          email: "dlafleur@fakemail.com",
+          password_digest: "example",
+          is_seller: true,
+          seller_postcode: "H2T 2L1"
+        },
+        {
+          first_name: "Albert",
+          last_name: "Jacques",
+          email: "ajacques@fakemail.com",
+          password_digest: "example",
+          is_seller: true,
+          seller_postcode: "H2J 1E5"
+        },
+        {
+          first_name: "Avril",
+          last_name: "Devereux",
+          email: "adevereux@fakemail.com",
+          password_digest: "example",
+          is_seller: true,
+          seller_postcode: "H2A 3M4"
+        }
+      ]);
+    });
 };
