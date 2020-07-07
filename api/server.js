f1d8a34e8b260fe4c425a00da9136b46beca0381
Michael Hallett <m.hallett@gmail.com>
2020-07-07 11:05:06 -0400

    Added routes for new routes

--- api/server.js
+++ api/server.js
@@ -6,6 +6,8 @@ const logger = require('morgan');
 const usersRouter = require('./routes/users');
 const menu_itemsRouter = require('./routes/menu_items');
 const allergensRouter = require('./routes/allergens');
+const loginRouter = require('./routes/session');
+const registerRouter = require('./routes/register');
 
 const app = express();
 const knex = require('./knex/knex.js');
@@ -20,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/users', usersRouter);
 app.use('/api/menu_items', menu_itemsRouter);
 app.use('/api/allergens', allergensRouter);
+app.use('/api/login', loginRouter);
+app.use('/api/register', registerRouter);
 
 app.get("/api", (req, res, next) => {
   res.send({ message: "We did it!" });
