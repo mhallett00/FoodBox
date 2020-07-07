8cb548ec21f5cf5c3e3eb1501c55077cc3487108
S0l0kr3w <44209188+S0l0kr3w@users.noreply.github.com>
2020-07-07 14:56:16 -0400

    react router dom installed, renamed web app FOODBOX, Main & CardDecks components created

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, Fragment } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import axios from "axios";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import {
@@ -10,13 +9,11 @@ import {
   Link
 } from "react-router-dom";
 import Homepage from './Homepage';
+import Navigation from './Navigation';
 import About from './About';
 import Menu from './Menu';
 import Register from './register';
 import Login from './Login';
-
-
-
 
 export default function App() {
   //  const [show, setShow] = useState(REGISTER);
@@ -38,25 +35,9 @@ export default function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <header className="App-header">
-            <div className="header-title">
-              <div className="logo">
-                <Link to="/">F O O D B O X</Link>
-              </div>
-                <Link className="react-links" to="/Homepage">Home</Link>
-                <Link className="react-links" to="/about">About</Link>
-                <Link className="react-links" to="/menu">Menu</Link>
-                <Link className="react-links" to="/login">Login</Link>
-              </div>
-
-              <div className="App">
-                <Login />
-              </div>
-              
-          </header>
-        </nav>
-
+        <Navigation/>
+          <div className="App">
+          </div>
         {/* A <Switch> looks through its children <Route>s and
         renders the first one that matches the current URL. */}
 
@@ -67,7 +48,6 @@ export default function App() {
             // dispatch={dispatch}
             // profileInfo={state.profileInfo}
           />
-            
           </Route>
           <Route path="/menu">
             <Menu />
--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,10 +1,14 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CardDecks from "./CardDecks";
+import Main from "./Main";
+
 
 export default function Homepage() {
   return (
   <div>
-  <h2>Homepage!!!!</h2>
-  <p>Bacon ipsum dolor amet strip steak kielbasa fatback venison flank, prosciutto porchetta leberkas chuck sirloin boudin. Short loin rump pork belly meatball, kevin beef pork chop tail fatback. Ribeye alcatra pancetta pig chislic bresaola beef ribs pastrami ham hock pork chop ham biltong doner capicola. Prosciutto andouille picanha, chicken shoulder leberkas fatback meatball cupim buffalo ball tip bacon pastrami pork loin spare ribs. Ham chuck sausage short loin salami pancetta bacon buffalo t-bone picanha alcatra beef. Pig drumstick ground round ham jerky, pork porchetta pork loin.</p>
+    <Main/>
+    <CardDecks/>
   </div>
   )
 }
