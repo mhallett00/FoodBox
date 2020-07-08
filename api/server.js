653f35396198fc2fe1f7baeee201b71e240ea519
Michael Hallett <m.hallett@gmail.com>
2020-07-07 20:17:09 -0400

    Cleaned up routes

--- api/server.js
+++ api/server.js
@@ -5,25 +5,21 @@ const logger = require('morgan');
 
 const usersRouter = require('./routes/users');
 const menu_itemsRouter = require('./routes/menu_items');
-const allergensRouter = require('./routes/allergens');
-const loginRouter = require('./routes/session');
-const registerRouter = require('./routes/register');
+const sessionRouter = require('./routes/session');
 
 const app = express();
 const knex = require('./knex/knex.js');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/api/users', usersRouter);
 app.use('/api/menu_items', menu_itemsRouter);
-app.use('/api/allergens', allergensRouter);
-app.use('/api/login', loginRouter);
-app.use('/api/register', registerRouter);
+app.use('/api/session', sessionRouter);
 
 app.get("/api", (req, res, next) => {
   res.send({ message: "We did it!" });
