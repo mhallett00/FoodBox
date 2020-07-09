ef7b9b1da6f481c52a8dee67314e351344b57f81
Michael Hallett <m.hallett@gmail.com>
2020-07-08 21:32:55 -0400

    Added orders route to server

--- api/server.js
+++ api/server.js
@@ -7,6 +7,7 @@ const usersRouter = require('./routes/users');
 const menu_itemsRouter = require('./routes/menu_items');
 const sessionsRouter = require('./routes/session');
 const cartsRouter = require('./routes/carts');
+const ordersRouter = require('./routes/orders');
 
 const app = express();
 const knex = require('./knex/knex.js');
@@ -22,6 +23,7 @@ app.use('/api/users', usersRouter);
 app.use('/api/menu_items', menu_itemsRouter);
 app.use('/api/sessions', sessionsRouter);
 app.use('/api/carts', cartsRouter);
+app.use('/api/orders', ordersRouter);
 
 app.get("/api", (req, res, next) => {
   res.send({ message: "We did it!" });
