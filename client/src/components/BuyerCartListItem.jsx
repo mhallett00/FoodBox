327a84f2214ba0b429613bc815cf237cd667b6f4
S0l0kr3w <44209188+S0l0kr3w@users.noreply.github.com>
2020-07-10 14:13:28 -0400

    passed created props from BuyerCart component

--- client/src/components/BuyerCartListItem.jsx
+++ client/src/components/BuyerCartListItem.jsx
@@ -1,26 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
-export default function BuyerCartListItem() {
+
+export default function BuyerCartListItem(props) {
+  const { name, image, user_id, seller_name, quantity, description, price_cents } = props;
+  const [itemCount, setItemCount] = useState(1);
+
   return (
+    
     <tr>
-      <td><img src=""/>Image Here</td>
+      <td><img src=""/>{image}</td>
       <td>
-        <h3>Mama Beth's Spring Rolls</h3>
+        <h3>{seller_name}'s {name}</h3>
         <h5>Single order - 6 pieces</h5>
         <ButtonGroup aria-label="Basic example">
-          <Button variant="light">-</Button>
-          <Button variant="light">quantity</Button>
-          <Button variant="light">+</Button>
+          <Button variant="light" onClick={() => setItemCount(itemCount - 1)}>-</Button>
+          <Button variant="light">{itemCount}</Button>
+          <Button variant="light" onClick={() => setItemCount(itemCount + 1)}>+</Button>
         </ButtonGroup>
         <br/>
         <Button variant="dark" type="submit">
           Remove
         </Button>
       </td>
-      <td colSpan="2">Food Description Here</td>
-      <td>$ price/unit</td>
+      <td colSpan="2">{description}</td>
+      <td>${itemCount * price_cents}</td>
     </tr>
+    
   );
 }
