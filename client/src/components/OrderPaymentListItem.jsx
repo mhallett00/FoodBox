8e034aee4273c87f8e11decd1c472015e59271d5
Michael Hallett <m.hallett@gmail.com>
2020-07-11 20:06:22 -0400

    began dynamic content implementation

--- client/src/components/OrderPaymentListItem.jsx
+++ client/src/components/OrderPaymentListItem.jsx
@@ -4,12 +4,15 @@ import React from "react";
 export default function OrderPaymentListItem(props) {
   const { item_name, seller_fn, seller_ln, image, id, description, price, quantity } = props;
 
+  console.log(props)
+
   return (
     <tr>
-      <td><img src=""/>{image}</td>
+      <td><img src={image} width="120vw" heigh="90vh"/></td>
       <td>
         <h3>{seller_fn} {seller_ln}'s {item_name}</h3>
-        <h5>Quantity</h5>
+      </td>
+      <td>
         <h6>{quantity}</h6>
       </td>
       <td colSpan="2">{description}</td>
