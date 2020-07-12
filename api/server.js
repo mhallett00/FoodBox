a26b4cd58b908c828811acc95423b97eccdc4269
Michael Hallett <m.hallett@gmail.com>
2020-07-12 15:40:07 -0400

    added stripe handling

--- api/server.js
+++ api/server.js
@@ -2,15 +2,18 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+require('dotenv').config()
 
 const usersRouter = require('./routes/users');
 const menu_itemsRouter = require('./routes/menu_items');
 const sessionsRouter = require('./routes/session');
 const cartsRouter = require('./routes/carts');
 const ordersRouter = require('./routes/orders');
-
 const app = express();
 const knex = require('./knex/knex.js');
+
+// STRIPE PAYMENT
+const stripe = require('stripe')('sk_test_51GxIzcKCiGdjBgOYA94niR4KNH77MR2E71AXqgcnxkPnozjjY8ggyX4jSu0hClkxjuaVOVpzT05KfuQQlZv5TWtO00z3Wni5tQ');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -27,6 +30,23 @@ app.use('/api/orders', ordersRouter);
 
 app.get("/api", (req, res, next) => {
   res.send({ message: "We did it!" });
+});
+
+app.post('/api/payment', async (req, res) => {
+  const { id, amount } = req.body
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount,
+    currency: 'CAD',
+    description: 'Your order',
+    payment_method: id,
+    confirm: true
+  });
+  console.log(paymentIntent),
+  res
+  .status(200)
+  .json({
+    confirm: "Success!"
+  });
 });
 
 module.exports = app;
