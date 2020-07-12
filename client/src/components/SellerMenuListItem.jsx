da846db434534d30dbc8ab973a50634c7d2b8e97
Michael Hallett <m.hallett@gmail.com>
2020-07-12 15:44:00 -0400

    temporarily removed buttons and destructured item data

--- client/src/components/SellerMenuListItem.jsx
+++ client/src/components/SellerMenuListItem.jsx
@@ -5,13 +5,23 @@ import Button from 'react-bootstrap/Button';
 
 export default function SellerMenuListItem(props) {
 
+  const { 
+    image, 
+    seller_fn, 
+    seller_ln, 
+    item_name, 
+    onDelete, 
+    description, 
+    price 
+  } = props;
+  
   return (
     <tr>
-      <td><img src={props.image} width="120vw" heigh="90vh"/></td>
+      <td><img src={image} width="120vw" heigh="90vh"/></td>
       <td>
-        <h3>{props.seller_fn} {props.seller_ln}'s {props.item_name}</h3>
+        <h3>{seller_fn} {seller_ln}'s {item_name}</h3>
         <h5></h5>
-        <form action="/register method=POST" className="menu-availability">
+        {/* <form action="/register method=POST" className="menu-availability">
         <h5>Available to Order?</h5>
         <div className="custom-control custom-radio">
             <input type="radio" id="customRadio1" name="customRadio" className="custom-control-input"/>
@@ -21,16 +31,16 @@ export default function SellerMenuListItem(props) {
             <input type="radio" id="customRadio2" name="customRadio" className="custom-control-input"/>
             <label className="custom-control-label" htmlFor="customRadio2">No</label>
           </div>
-        </form>
-        <Button variant="dark" href="/seller_menu/edit_item" type="submit">
+        </form> */}
+        {/* <Button variant="dark" href="/seller_menu/edit_item" type="submit">
           Edit Item
-        </Button>
-        <Button variant="danger" type="submit" onClick={props.onDelete}>
+        </Button> */}
+        <Button variant="danger" type="submit" onClick={onDelete}>
           Delete Item 
         </Button>
       </td>
-      <td colSpan="2">{props.description}</td>
-      <td>$ {props.price / 100}</td>
+      <td colSpan="2">{description}</td>
+      <td>$ {price / 100}</td>
     </tr>
   );
 }
