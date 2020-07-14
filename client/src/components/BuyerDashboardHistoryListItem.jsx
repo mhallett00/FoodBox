8f93d89cfddb9818d3036cc538ec8b2ddf0555ff
Michael Hallett <m.hallett@gmail.com>
2020-07-14 15:49:05 -0400

    Fixed display variables, and props

--- client/src/components/BuyerDashboardHistoryListItem.jsx
+++ client/src/components/BuyerDashboardHistoryListItem.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
 import OHFrag from './OHFrag'
 
 export default function BuyerDashboardHistoryListItem(props) {
 
-  const { menuItems, order_id, order_total } = props;
+  const { menuItems, order_id, order_total, seller_fn, seller_ln, order_quantity } = props;
 
   const orderfragment = menuItems
   ? menuItems.map((menuItem, index) => {
     return(
       <OHFrag
-        key={index}
-        image={menuItem.image}
-        item_name={menuItem.name}
-        item_quantity={menuItem.order_quantity}
+      key={index}
+      image={menuItem.image}
+      item_name={menuItem.name}
+      item_quantity={order_quantity.cartiteminfo[index].order_quantity}
       />
     );
   })
@@ -23,7 +22,8 @@ export default function BuyerDashboardHistoryListItem(props) {
       <tr>
         <td >{order_id}</td>
         <td>{orderfragment}</td>
-        <td>{order_total}</td>
+        <td>{seller_fn} {seller_ln}</td>
+        <td>$ {(order_total / 100).toFixed(2)}</td>
       </tr>
   );
 }
