252dccd9177a6c23b05edc34d94e1c19648366f5
Michael Hallett <m.hallett@gmail.com>
2020-07-14 15:59:39 -0400

    Created component to display items for seller history

---
+++ client/src/components/OHFragSeller.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+export default function OHFragSeller (props) {
+
+  const { item_name, image, item_quantity} = props;
+  return (
+    <tr>
+      <td><img src={image} width="40vw" heigh="30vh"/></td>
+      <td>
+        <h5>{item_name}</h5>
+      </td>
+      <td>
+        <h5>x {item_quantity}</h5>
+      </td>
+
+    </tr>
+  );
+} 
