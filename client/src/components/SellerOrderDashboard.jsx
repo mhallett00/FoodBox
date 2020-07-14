6db0fb39567c6d981f35865bf7b48616800badd9
Michael Hallett <m.hallett@gmail.com>
2020-07-14 15:55:30 -0400

    implmeented order confirmation for seller

--- client/src/components/SellerOrderDashboard.jsx
+++ client/src/components/SellerOrderDashboard.jsx
@@ -8,9 +8,7 @@ export default function SellerOrderDashboard(props) {
   const [ activeOrders, setActiveOrders ] = useState();
 
   const { 
-    id,
-    first_name,
-    last_name
+    id
   } = props.userData;
 
   const getActiveOrders= (user_id) => {
@@ -20,8 +18,22 @@ export default function SellerOrderDashboard(props) {
     })
     .catch((err) => {
       console.log(err)
+    }); 
+  }
+
+  const confirmDelivery = (order_id) => {
+    const newActiveOrders = activeOrders.filter((order) => {
+     return order.id !== order_id;
     });
     
+    axios.post(`api/orders/${order_id}`)
+    .then(() => {
+      setActiveOrders(newActiveOrders)
+      // getActiveOrders(id)
+    })
+    .catch((err) => {
+      console.log(err)
+    });
   }
 
   useEffect(() => {
@@ -34,18 +46,21 @@ export default function SellerOrderDashboard(props) {
         <SellerOrderDashboardListItem
         key={index}
         menuItems={orderId.menu_items}
+        buyer_fn={orderId.userinfo[0].first_name}
+        buyer_ln={orderId.userinfo[0].last_name}
         order_id={orderId.id}
-        order_total={orderId.order_total}
+        order_quantity={orderId}
+        order_total={orderId.total}
+        onConfirm={() => confirmDelivery(orderId.id)}
         />
       );
     })
     : "No menu!";  
 
-    console.log("activeorders", activeOrders)
   return (
     <>
       <div className='seller-dashboard'>
-        <h2>{first_name}'s Dashboard</h2>
+        <h2>Your Menu's Dashboard</h2>
       </div>
       <div className="seller-dashboard list">
         <Table striped borderless hover>
@@ -54,12 +69,12 @@ export default function SellerOrderDashboard(props) {
               <th>Order Id</th>
               <th>Items</th>
               <th>Customer Name</th>
-              <th>Delivery Address</th>
-              <th>Order Fulfilled</th>
+              <th>Order Total</th>
+              <th>Fulfill Order</th>
             </tr>
           </thead>
           <tbody>
-            <SellerOrderDashboardListItem/>
+            {upcomingOrders}
           </tbody>
         </Table>
       </div>
